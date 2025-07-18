67ed0f8e8b13f281cfd09f69771d8ef0d676f8f3
Lx <116142186+0AIB@users.noreply.github.com>
2025-07-19 00:43:33 +0530

    Update config.py

--- config.py
+++ config.py
@@ -1,8 +1,8 @@
 import os
 class Config:   
-    APP_ID = int(os.environ.get("APP_ID", "21288218"))
-    API_HASH = os.environ.get("API_HASH", "dd47d5c4fbc31534aa764ef9918b3acd")
-    TG_BOT_TOKEN = os.environ.get("TG_BOT_TOKEN", "") 
-    OWNER_ID = set(int(x) for x in os.environ.get("OWNER_ID", "").split())
+    APP_ID = int(os.environ.get("APP_ID"))
+    API_HASH = os.environ.get("API_HASH")
+    TG_BOT_TOKEN = os.environ.get("TG_BOT_TOKEN") # "6658841062:AAFDWIgbflwa0sedHXoXqycHL_Mnz85h7TU") 
+    OWNER_ID = os.environ.get("OWNER_ID", "")
     DATABASE_URI = os.environ.get("DATABASE_URI", "")
-    TO_CHANNEL = int(os.environ.get('TO_CHANNEL', -1001819186027))
+    TO_CHANNEL = int(os.environ.get("APP_ID"))
